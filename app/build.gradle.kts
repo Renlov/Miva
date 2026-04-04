3fccb00374c68161f75147db75d598da5c0cea88
Renlov <a.pimenov95@mail.ru>
2026-04-04 08:40:18 +0300

    add firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.firebase.bom)
-    implementation(libs.firebase.analitics)
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
